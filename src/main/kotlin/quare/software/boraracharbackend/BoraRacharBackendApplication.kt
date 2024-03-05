670bc854b386ea451ee432fb8e3a0aa85892f9df
fabriciokjc <fabriciokjc@gmail.com>
2024-03-05 00:38:11 -0300

    base projeto kotlin

---
+++ src/main/kotlin/quare/software/boraracharbackend/BoraRacharBackendApplication.kt
@@ -0,0 +1,11 @@
+package quare.software.boraracharbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoraRacharBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BoraRacharBackendApplication>(*args)
+}
